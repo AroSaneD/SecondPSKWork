622df403e656ff138cbc32e5df0e1397eb1980f1
Arūnas Seniucas <Arnas Seniucas>
2016-05-10 00:29:41 +0300

    Revamped conversation

--- src/java/components/RegistrationComponent.java
+++ src/java/components/RegistrationComponent.java
@@ -7,21 +7,40 @@ package components;
 
 import Entities.Player;
 import java.io.Serializable;
+import java.util.UUID;
 import javax.ejb.Stateful;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.SynchronizationType;
 import managers.GameManager;
 import managers.PlayerManager;
+import org.hsqldb.Server;
 
 /**
  *
  * @author Aron
  */
 @Named
-@SessionScoped
+//@SessionScoped
+@ConversationScoped
+@Stateful
 public class RegistrationComponent implements Serializable {
+    
+    @PersistenceContext(type = PersistenceContextType.EXTENDED,
+            synchronization = SynchronizationType.UNSYNCHRONIZED,
+            unitName = "SecondJavaWorkPU")
+    private EntityManager em;
+    
+    @Inject
+    private Conversation conversation;
     
     @Inject 
     private GameManager gameManager;
@@ -34,6 +53,8 @@ public class RegistrationComponent implements Serializable {
     private String playerName = null;
     private String errorMessage = null;
 
+    private Player player = new Player();
+    
     public String getErrorMessage() {
         return errorMessage;
     }
@@ -61,6 +82,12 @@ public class RegistrationComponent implements Serializable {
             return "failure";
         }
         else{
+            conversation.begin();
+            player.setEmail(email);
+            player.setPassword(password);
+            player.setUid(UUID.randomUUID().toString());
+            player.setPlayername(UUID.randomUUID().toString());
+            em.persist(player);
             return "success";
         }
     }
@@ -72,10 +99,27 @@ public class RegistrationComponent implements Serializable {
         }
         else{
             //end scope somehow
-            Player player = playerManager.createPlayer(email, password, playerName);
+            //Player player = playerManager.createPlayer(email, password, playerName);
             
             if(player != null){
-                this.gameManager.setPlayer(player);
+                
+                try{
+                    player.setPlayername(playerName);
+                    em.persist(player); //Probably not necessary, but not sure enough
+                    em.joinTransaction();
+                    em.flush();
+                    conversation.end();
+                }
+                catch(OptimisticLockException ole){
+                    return "criticalFailure";
+                }
+                
+                try{
+                    this.gameManager.setPlayer(player);
+                }
+                catch(Exception e){
+                    
+                }
                 return "success";
             }
             else{
