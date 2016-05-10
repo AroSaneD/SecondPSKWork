f7c93cde35c154c209608d43bc928b63d31d3a64
Arūnas Seniucas <Arnas Seniucas>
2016-05-10 09:07:00 +0300

    Full commit

--- build/web/WEB-INF/classes/SQL/DBCreation.sql
+++ build/web/WEB-INF/classes/SQL/DBCreation.sql
@@ -8,6 +8,7 @@
  * Created: Apr 26, 2016
  */
 DROP TABLE GameMatch;
+DROP TABLE PlayerSession;
 DROP TABLE Player;
 
 
@@ -19,7 +20,7 @@ UID                 VARCHAR(255)            UNIQUE NOT NULL,
 PlayerName          VARCHAR(255)            UNIQUE NOT NULL,
 PlayerPoints        INTEGER                 NOT NULL DEFAULT 0,
 Password            VARCHAR(255)            NOT NULL,
-Email               VARCHAR(255)            NOT NULL,
+Email               VARCHAR(255)            UNIQUE NOT NULL,
 
 PRIMARY KEY (ID)
 );
@@ -30,13 +31,25 @@ CREATE TABLE GameMatch
 ID                  INTEGER                 GENERATED ALWAYS AS IDENTITY
                                             (START WITH 1, INCREMENT BY 1),
 PlayerOne           INTEGER                 NOT NULL,
-PlayerTwo           INTEGER                 NOT NULL,
+PlayerTwo           INTEGER                 ,
 VictoriousPlayer    INTEGER                 ,
-PlayerOneScore      DECIMAL                 NOT NULL,
-PlayerTwoScore      DECIMAL                 NOT NULL,
+PlayerOneScore      DECIMAL                 ,
+PlayerTwoScore      DECIMAL                 ,
 DatePlayed          DATE                    NOT NULL,
 
 PRIMARY KEY (ID),
 FOREIGN KEY (PlayerOne) REFERENCES Player(ID),
 FOREIGN KEY (PlayerTwo) REFERENCES Player(ID)
+);
+
+
+CREATE TABLE PlayerSession
+(
+ID                  INTEGER                 GENERATED ALWAYS AS IDENTITY
+                                            (START WITH 1, INCREMENT BY 1),
+Player              INTEGER                 NOT NULL,
+LoginDate           TIMESTAMP               NOT NULL,
+
+PRIMARY KEY (ID),
+FOREIGN KEY (Player) REFERENCES Player(ID)
 );
